ebb3b3c5c3d70125901e2adbfc50a2d173474bd4
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-02-10 10:42:43 -0800

    list process names

--- ps.go
+++ ps.go
@@ -2,27 +2,73 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"syscall"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
 
+const (
+	PROCESS_VM_READ = 0x10
+)
+
 var psapi = windows.NewLazySystemDLL("psapi.dll")
 var procEnumProcesses = psapi.NewProc("EnumProcesses")
+var procEnumProcessModules = psapi.NewProc("EnumProcessModules")
+var procGetModuleBaseName = psapi.NewProc("GetModuleBaseNameW")
+
+func errno(e1 error) error {
+	if e1, ok := e1.(syscall.Errno); ok && e1 == 0 {
+		e1 = syscall.EINVAL
+	}
+	return e1
+}
 
 func EnumProcesses(pids []uint32) (n int, err error) {
 	if len(pids) == 0 {
 		return 0, nil
 	}
 	var outsize uint32
-	_, _, err = procEnumProcesses.Call(
+	r1, _, e1 := procEnumProcesses.Call(
 		uintptr(unsafe.Pointer(&pids[0])),
 		uintptr(len(pids))*unsafe.Sizeof(pids[0]),
 		uintptr(unsafe.Pointer(&outsize)),
 	)
-	n = int(uintptr(outsize) / unsafe.Sizeof(pids[0]))
+	if r1 == 0 {
+		err = errno(e1)
+	} else {
+		n = int(uintptr(outsize) / unsafe.Sizeof(pids[0]))
+	}
 	return n, err
+}
+
+func EnumProcessModule(process windows.Handle) (h windows.Handle, err error) {
+	var hmodule windows.Handle
+	var needed int32
+	r1, _, e1 := procEnumProcessModules.Call(
+		uintptr(process),
+		uintptr(unsafe.Pointer(&hmodule)),
+		4,
+		uintptr(unsafe.Pointer(&needed)),
+	)
+	if r1 == 0 {
+		err = errno(e1)
+		return 0, err
+	}
+	return hmodule, nil
+}
+
+func GetModuleBaseName(process windows.Handle, module windows.Handle, outString *uint16, size uint32) (n int, err error) {
+	r1, _, e1 := procGetModuleBaseName.Call(
+		uintptr(process),
+		uintptr(module),
+		uintptr(unsafe.Pointer(outString)),
+		uintptr(size),
+	)
+	if r1 == 0 {
+		return 0, errno(e1)
+	}
+	return int(r1), nil
 }
 
 func main() {
@@ -33,19 +79,43 @@ func main() {
 		return
 	}
 	pids = pids[:n]
-	sort.Sort(Uint32Slice(pids))
+	//sort.Sort(Uint32Slice(pids))
 	for _, id := range pids {
-		printInfo(pid)
+		if err := printInfo(id); err != nil {
+			fmt.Println(id, err)
+		}
 	}
 }
 
-func printInfo(pid int32) error {
-	h, err := windows.OpenProcess(windows.PROCESS_QUERY_INFORMATION, false, pid)
+type SyscallError struct {
+	call string
+	err  error
+}
+
+func (e *SyscallError) Error() string {
+	return fmt.Sprintf("%s: %v", e.call, e.err)
+}
+
+func printInfo(pid uint32) error {
+	h, err := windows.OpenProcess(windows.PROCESS_QUERY_INFORMATION|PROCESS_VM_READ, false, pid)
 	if err != nil {
-		return err
+		return &SyscallError{"OpenProcess", err}
 	}
 	defer windows.CloseHandle(h)
-	fmt.Println()
+	mod, err := EnumProcessModule(h)
+	if err != nil {
+		return &SyscallError{"EnumProcessModule", err}
+	}
+	defer windows.CloseHandle(mod)
+	//fmt.Println(mod, err)
+	var s = make([]uint16, 255)
+	n, err := GetModuleBaseName(h, mod, &s[0], uint32(len(s)))
+	if err != nil {
+		return &SyscallError{"GetModuleBaseName", err}
+	}
+	//fmt.Println(n, s[:n])
+	fmt.Println(pid, windows.UTF16ToString(s[:n]))
+	return nil
 }
 
 type Uint32Slice []uint32
