4bcbec31cbfce001be8f52262d9e699348ddecbf
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-02-10 15:45:38 -0800

    print modules

--- ps.go
+++ ps.go
@@ -21,9 +21,16 @@ func main() {
 			fmt.Println(id, err)
 		} else {
 			fmt.Println(id, name)
+			printModules(id)
 		}
 	}
 }
+
+type Uint32Slice []uint32
+
+func (s Uint32Slice) Len() int           { return len(s) }
+func (s Uint32Slice) Less(i, j int) bool { return s[i] < s[j] }
+func (s Uint32Slice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 
 type SyscallError struct {
 	call string
@@ -54,8 +61,34 @@ func getProcessName(pid uint32) (string, error) {
 	return windows.UTF16ToString(s[:n]), nil
 }
 
-type Uint32Slice []uint32
-
-func (s Uint32Slice) Len() int           { return len(s) }
-func (s Uint32Slice) Less(i, j int) bool { return s[i] < s[j] }
-func (s Uint32Slice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func printModules(pid uint32) error {
+	h, err := windows.OpenProcess(windows.PROCESS_QUERY_INFORMATION|PROCESS_VM_READ, false, pid)
+	if err != nil {
+		return &SyscallError{"OpenProcess", err}
+	}
+	defer windows.CloseHandle(h)
+	modules := make([]windows.Handle, 255)
+	n, err := EnumProcessModules(h, modules)
+	if err != nil {
+		return &SyscallError{"EnumProcessModules", err}
+	}
+	if n < len(modules) {
+		modules = modules[:n]
+	}
+	defer func() {
+		for _, mod := range modules {
+			windows.CloseHandle(mod)
+		}
+	}()
+	var buf = make([]uint16, 255)
+	for i, mod := range modules {
+		//fmt.Println(mod, err)
+		n, err := GetModuleBaseName(h, mod, &buf[0], uint32(len(buf)))
+		if err != nil {
+			return &SyscallError{"GetModuleBaseName", err}
+		}
+		s := windows.UTF16ToString(buf[:n])
+		fmt.Printf("\t%d: %s\n", i, s)
+	}
+	return nil
+}
